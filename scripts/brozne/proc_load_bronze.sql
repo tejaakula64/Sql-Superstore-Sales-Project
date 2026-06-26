74b281a0e2a12e52703b5354044a0742d355a325
tejaakula64 <tejaakula64@gmail.com>
2026-06-26 10:16:42 +0530

    Update proc_load_bronze.sql

--- scripts/brozne/proc_load_bronze.sql
+++ scripts/brozne/proc_load_bronze.sql
@@ -13,11 +13,11 @@ Parameters:
    This stored procedure does not accept any parameters or return any values. 
 
 Usage Example:
-	EXEC bronze.load_sales_bronze;
+	EXEC bronze.load_bronze;
 ======================================================================
 */
 
-CREATE OR ALTER PROCEDURE bronze.load_sales_bronze AS 
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS 
 BEGIN
 	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
 	BEGIN TRY 
